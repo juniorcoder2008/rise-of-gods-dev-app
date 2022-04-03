8edf745554cfe0b51227ca02037f7da504c32f4d
JuniorCoder <thomas08samuel@gmail.com>
2022-04-03 15:19:22 +0200

    better UX

--- frontend/src/components/CreateChangelog.jsx
+++ frontend/src/components/CreateChangelog.jsx
@@ -21,8 +21,8 @@ const CreateChangelog = () => {
     <div className='ml-10 mt-10'>
       <h1 className='text-xl font-bold mb-4'>Neuen Changelog erstellen</h1>
       <form className='flex flex-col gap-3' onSubmit={createNewChangelog}>
-        <input type="text" className='input' placeholder='Version' ref={version} />
-        <textarea className='text-area resize-none' placeholder='Changelogs' ref={content}></textarea>
+        <input type="text" className='input' placeholder='Version (z.B. 0.4.2)' ref={version} required />
+        <textarea maxLength={2000} className='text-area resize-none' placeholder='Changelogs' ref={content} required ></textarea>
         <button className='btn-success'>Erstellen</button>
       </form>
     </div>
